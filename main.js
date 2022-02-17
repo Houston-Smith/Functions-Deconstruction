7e0d5afcea071e1f3935a1937715f6f8f24dcca7
Houston-Smith <houstonbsmith@gmail.com>
2022-02-17 13:03:11 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const peelApples = () => {
+  console.log("Peel four medium sized apples.")
+}
+
+const chopApples = () => {
+  console.log("Cut apples into thin slices. (Abt 1/4 in. thick)")
+}
+
+const caramelizeApples = () => {
+  console.log("Caramelize apples over medium heat for about 5 minutes. Add 1/4 cup of brown sugar and 1 tspn of cinnimon.")
+}
+
+const mixOatmeal = () => {
+  console.log("In a large bowl, mix 3 cups of oatmeal, 2 scoops of protein powder, and 1 tsp of baking powder.")
+}
+
+const mixLiquids = () => {
+  console.log("In a medium bowl, mix 2 eggs, 2 cups of milk, and 1 tbspn of melted butter.")
+}
+
+const mixItUp = () => {
+  console.log("Combine your oatmeal mixture, bowl of liquids, and caramelized apples and stir thoroughly.")
+}
+
+const panPrep = () => {
+  console.log("Spray a 9 by 13 inch pan with non-stick spray. Spray it real good.")
+}
+
+const bakeIt = () => {
+  console.log("Pour your oatmealy mixture into your pan and bake it at 375 for 30 minutes.")
+}
+
+const eatIt = () => {
+  console.log("Baked Oatmeal. Enjoy the fruits of your labor.")
+}
+
+
+  peelApples();
+  chopApples();
+  caramelizeApples();
+  mixOatmeal();
+  mixLiquids();
+  mixItUp();
+  panPrep();
+  bakeIt ();
+  eatIt ();
